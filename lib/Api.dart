d4ecbe7b64b4351fd15cacbcd7113fa059560d86
math-rodr <59507657+math-rodr@users.noreply.github.com>
2020-10-25 23:10:38 -0300

    Add files via upload
    Modelo desenvolvido no curso sobre como utilizar a API do YouTube

---
+++ lib/Api.dart
@@ -0,0 +1,46 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+import 'package:youtube/model/Video.dart';
+
+const CHAVE_YOUTUBE_API = "AIzaSyCfbxGNcRRUwdDb0N88fQBGL7TzddNcGhQ";
+const ID_CANAL = "UCVHFbqXqoYvEWM1Ddxl0QDg";
+const URL_BASE = "https://www.googleapis.com/youtube/v3/";
+
+
+class Api {
+  Future<List<Video>> pesquisar(String pesquisa) async {
+
+    http.Response response = await http.get(
+      URL_BASE + "search"
+        "?part=snippet"
+        "&type=video"
+        "&maxResults=20"
+        "&order=date"
+        "&key=$CHAVE_YOUTUBE_API"
+        "&channelId=$ID_CANAL"
+        "&q=$pesquisa"
+        );
+
+        if (response.statusCode == 200){
+            
+          Map<String, dynamic> dadosJson = json.decode( response.body );
+          
+          List<Video> videos = dadosJson["items"].map<Video>(
+            (map){
+                return Video.fromJson(map);
+            }
+          ).toList();
+          
+          return videos;
+
+          /*for(var video in videos){
+            print("Resultado: " + video.titulo);
+          }*/
+          //print("resultado: " + dadosJson["items"][0]["snippet"]["title"].toString());
+        
+        }else{
+
+        }
+  }
+}
